b9c28a359af07b3afadeb7004d69a2489e7226bb
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2022-08-13 17:52:10 +0530

    test: update to match the current API

--- test/aksharas.test.ts
+++ test/aksharas.test.ts
@@ -1,15 +1,15 @@
 import { expect, describe, test } from "@jest/globals";
 
-import aksharas from "../src";
+import Aksharas from "../src";
 import { testData } from "./test-data";
 
 describe("Aksharas#tokenize", () => {
   test.each(testData)(
     "should produce the expected tokens for input: '$input'",
     ({ input, expectedTokens }) => {
-      const tokens = aksharas.tokenize(input);
+      const results = Aksharas.analyse(input);
 
-      tokens.forEach((token, i) => {
+      results.all.forEach((token, i) => {
         const expectedToken = expectedTokens[i];
 
         expect(token.type).toEqual(expectedToken.type);

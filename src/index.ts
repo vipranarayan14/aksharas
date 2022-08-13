f7e6705c2ff74c8ae336e4f61ef2afef1beb8f49
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2022-08-13 17:49:06 +0530

    feat!: make Aksharas API more friendly
    How?
    Replace `Aksharas.tokenize` with `Aksharas.analyse`. The former returned
    a list of `Token`-s whereas current API returns a `Results` object that
    gives easy and direct access to the aksharas and symbols as well as the
    whitespace, invalid and unrecognised chars in the input string.

--- src/index.ts
+++ src/index.ts
@@ -1,181 +1,62 @@
-import { Char } from "./char";
-import { Token, TokenAttributes, TokenType } from "./token";
+import { Token, TokenType } from "./token";
+import { tokenize } from "./tokenize";
 
-enum State {
-  Initial,
-  Vowel,
-  Consonant,
-  ConjunctConsonant,
-}
-
-const tokenize = (input: string): Token[] => {
-  const tokens: Token[] = [];
-
-  let pos = 0;
-  let acc = "";
-  let varnasLength: number = 0;
-  let state = State.Initial;
-
-  const resetVariables = () => {
-    pos = 0;
-    acc = "";
-    varnasLength = 0;
-    state = State.Initial;
-  };
-
-  const createToken = (tokenType: TokenType, attributes?: TokenAttributes) => {
-    tokens.push(new Token(tokenType, acc, pos, attributes));
-    resetVariables();
-  };
-
-  for (let i = 0, l = input.length; i < l; i += 1) {
-    const char = new Char(input[i]);
-
-    const nextChar = new Char(input[i + 1]);
-
-    acc += char.value;
-
-    switch (state) {
-      case State.Initial: {
-        pos = i;
-
-        if (char.isSymbol()) {
-          createToken(TokenType.Symbol);
-          break;
-        }
-
-        if (char.isWhitespace()) {
-          createToken(TokenType.Whitespace);
-          break;
-        }
-
-        if (char.isUnrecognised()) {
-          createToken(TokenType.Unrecognised);
-          break;
-        }
-
-        if (char.isVowel()) {
-          if (nextChar.isVowelAttachment()) {
-            state = State.Vowel;
-            break;
-          }
-
-          createToken(TokenType.Akshara, { varnasLength: 1 });
-          break;
-        }
-
-        if (char.isConsonant()) {
-          if (nextChar.isNukta() || nextChar.isConsonantAttachment()) {
-            varnasLength += 1;
-            state = State.Consonant;
-            break;
-          }
-
-          createToken(TokenType.Akshara, { varnasLength: 2 });
-          break;
-        }
-
-        createToken(TokenType.Invalid);
-        break;
-      }
-
-      case State.Vowel: {
-        if (char.isAccent()) {
-          createToken(TokenType.Akshara, { varnasLength: 1 });
-          break;
-        }
-
-        if (char.isYogavaha()) {
-          if (nextChar.isAccent()) {
-            break;
-          }
-
-          createToken(TokenType.Akshara, { varnasLength: 1 });
-          break;
-        }
-
-        break;
-      }
-
-      case State.Consonant: {
-        if (char.isNukta()) {
-          if (nextChar.isConsonantAttachment()) {
-            break;
-          }
-
-          createToken(TokenType.Akshara, { varnasLength: 2 });
-          break;
-        }
-
-        if (char.isVirama()) {
-          if (nextChar.isJoiner()) {
-            break;
-          }
-
-          if (nextChar.isConsonant()) {
-            state = State.ConjunctConsonant;
-            break;
-          }
-
-          createToken(TokenType.Akshara, { varnasLength });
-          break;
-        }
-
-        if (char.isJoiner()) {
-          if (nextChar.isJoiner()) {
-            break;
-          }
-
-          if (nextChar.isConsonant()) {
-            state = State.ConjunctConsonant;
-            break;
-          }
-
-          createToken(TokenType.Akshara, { varnasLength });
-          break;
-        }
-
-        if (char.isVowelMarkAttachment()) {
-          varnasLength += 1;
-
-          if (nextChar.isAccent()) {
-            break;
-          }
-
-          createToken(TokenType.Akshara, { varnasLength });
-          break;
-        }
-
-        if (char.isVowelMark()) {
-          if (nextChar.isVowelMarkAttachment()) {
-            break;
-          }
-
-          varnasLength += 1;
-          createToken(TokenType.Akshara, { varnasLength });
-          break;
-        }
-      }
-
-      case State.ConjunctConsonant: {
-        if (nextChar.isNukta() || nextChar.isConsonantAttachment()) {
-          varnasLength += 1;
-          state = State.Consonant;
-          break;
-        }
-
-        varnasLength += 2;
-        createToken(TokenType.Akshara, { varnasLength });
-        break;
-      }
-
-      default: {
-        break;
-      }
-    }
-  }
-
-  return tokens;
+type Results = {
+  all: Token[];
+  aksharas: Token[];
+  //   varnas: Token[] /* Yet to be implemented */;
+  symbols: Token[];
+  whitespaces: Token[];
+  chars: string[];
+  invalid: Token[];
+  unrecognised: Token[];
+  varnasLength: number /* Will be deprecated */;
 };
 
-export default { tokenize };
+const calcTotalVarnasLength = (tokens: Token[]) =>
+  tokens.reduce(
+    (total, akshara) =>
+      akshara.attributes ? total + akshara.attributes.varnasLength : total,
+    0
+  );
+
+const filterTokens = (tokens: Token[], tokenType: TokenType) =>
+  tokens.filter((token) => token.type === tokenType);
+
+const analyse = (input: string): Results => {
+  const tokens = tokenize(input);
+
+  const aksharas = filterTokens(tokens, TokenType.Akshara);
+
+  //   const varnas = [];
+
+  const chars = input.split("");
+
+  const symbols = filterTokens(tokens, TokenType.Symbol);
+
+  const invalidChars = filterTokens(tokens, TokenType.Invalid);
+
+  const whitespaces = filterTokens(tokens, TokenType.Whitespace);
+
+  const unrecognisedChars = filterTokens(tokens, TokenType.Unrecognised);
+
+  const varnasLength = calcTotalVarnasLength(aksharas);
+
+  return {
+    all: tokens,
+    aksharas: aksharas,
+    // varnas: varnas,
+    chars: chars,
+    symbols: symbols,
+    invalid: invalidChars,
+    whitespaces: whitespaces,
+    unrecognised: unrecognisedChars,
+    varnasLength: varnasLength,
+  };
+};
+
+const Aksharas = {
+  analyse,
+};
+
+export default Aksharas;
